da302cdcaed0c2e1a1593458b3b614c87d96dd79
Nathanael Lopes <nathanaelloepes50@gmail.com>
2024-11-05 14:43:05 -0100

    Update trabalho.cpp

--- trabalho.cpp
+++ trabalho.cpp
@@ -19,3 +19,27 @@ int main() {
     return 0;
 }
 
+
+//correção
+
+#iclude <iostrem>
+using namespace std;
+
+int numerospares[20] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+
+int main(){
+
+cout << "Esses são os numeros pares" << endl;
+
+    for(int x = 0; x < 20; x++){
+       if (numerospares[x] % 2 == 0){
+
+           cout << numerospares[20] << endl;
+           
+    }
+           
+
+
+
+return 0;
+}
