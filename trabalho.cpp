345744da7ba034379d9c6413e8e017204c84c203
Nathanael Lopes <nathanaelloepes50@gmail.com>
2024-11-05 14:42:05 -0100

    wdawdw

--- trabalho.cpp
+++ trabalho.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 int main() {
 
+    cout << "Esses são os numeros pares" << endl;
+
     int numerospares[20] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,
     20};
     
