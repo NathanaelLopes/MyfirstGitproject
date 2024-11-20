ba5f7214aeb9bd770f4651065ab75bc22a2c5175
Nathanael Lopes <nathanaelloepes50@gmail.com>
2024-11-20 10:35:14 -0100

    kbiautau

---
+++ contador.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+   // vou fazer um contador que começa a contar no 100 vai acabar no 100000 com incrementos de 2
+
+
+   for(int i = 100; i<=100000; i+=2){
+       cout << i << "\t";
+
+   }
+
+   
+    return 0;
+
+}
--- treino.cpp
+++ treino.cpp
@@ -2,3 +2,44 @@
 using namespace std;
 
 
+// string nome[2] = {"joana","filipe"};
+
+// int numrs[6] = {0,1,2,3,4,5};
+
+// string nome = "julio";
+
+// onst string nome = "julio";
+
+// cout << nome[2] << endl;
+
+int main(){
+
+   /* const string nome = "julio";
+
+    for(int i = 0; i<5; i++){
+        cout << nome[i] << " ";
+    }
+
+    cout << endl;
+   
+
+ // outra maneira
+
+    for(int i = 0 ; i < 10000; i++){
+        cout << "xpto" << "/t";
+
+    }
+
+    cout << endl;
+
+    */
+
+   for(int x = 1000; x<=2000; x+=10){
+       cout << x << " ";
+   }
+
+   cout << endl;
+
+
+
+}
