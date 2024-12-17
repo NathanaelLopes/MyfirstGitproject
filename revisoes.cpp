443442cc3994d8dd692e9b059f2236a9dc903a26
Nathanael Lopes <nathanaelloepes50@gmail.com>
2024-12-17 12:35:04 -0100

    oauyhf9afg

---
+++ revisoes.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int numero;
+    int num1;
+    float num2;
+    
+
+    
+    /*cout << "Diga um numero" << endl;
+    cin >> numero
+
+   if(numero % 5 == 0 || 5);*/
+
+   /*cout << "fale um numero" << endl;
+   cin >> numero;
+
+   cout << numero - 1 << endl;
+   cout << numero + 1 << endl;*/
+
+   /*cout << "informe um numero" << endl;
+   cin >> num;
+
+   cout << "o dobro da sua metade e = " << num << endl;*/
+
+   /*cout << "informe um numero" << endl;
+   cin >> num;
+
+   cout << "o triplo da metade e = " << (num / 2)*3 << endl;*/
+
+//    cout << "Diga um numero" << endl;
+//    cin >> num1;
+//    cout << "Diga um numero" << endl;
+//    cin >> num2;
+
+//    if(num1 < num2){
+//     cout << num2 << " e maior que " << num1 << endl;
+
+//    }else if (num2 < num1){
+//     cout << num1 << " e maior que " << num2;
+
+//    }else{
+//     cout << "sao iguais" << endl;
+//    }
+
+// cout << "diga-me um numero senhor" << endl;
+// cin >> num1;
+
+// cout << "o quadrado desse numero é = " << num1*num1 << endl;
+
+// int array5[3][3] ={ {1, 2, 3,},
+//                     {4, 5, 6,},
+//                     {7, 8, 9,} };
+
+//  for(int n = 0; n<3; n++){
+//     for(int x=0; x<3; x++){
+//         cout << array5[n][x] << " ";
+//     }
+//     cout << endl;
+//  }     
+
+int n = 0;
+
+cout << "digite um numero" << endl;
+cin >> n;
+
+while (n <= num1){
+cout << n << endl;
+n++;}
+
+
+    return 0;
+}
